d4f86818b6db8652dc8f4f72317ce5fd807cee49
DESKTOP-OA72ND6\Pictor05 <jirat.ter@gmail.com>
2020-12-27 15:49:10 +0700

    [Mod] set show character

---
+++ Project-A/Assets/Project-A/Scripts/Characters/BaseCharacter/Character.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Character : MonoBehaviour
+{
+    public CharacterAnimator characterAnimator;
+    public CharacterControl characterControl;
+
+    public UnityEvent<Vector3> onMove;
+
+    public abstract string GetAnimatorId();
+
+    protected virtual void Awake()
+    {
+        characterAnimator = GetComponentInChildren<CharacterAnimator>();
+    }
+
+    protected void DeregisterEvent()
+    {
+        onMove = null;
+    }
+}
---
+++ Project-A/Assets/Project-A/Scripts/Characters/BaseCharacter/CharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    private const string CHARACTER_MODEL_PATH = "Prefabs/Character/Models/";
+
+    protected Character character;
+
+    public void Init(Character character)
+    {
+        this.character = character;
+        GetModelCharacter();
+    }
+
+    private void GetModelCharacter()
+    {
+        string modeId = character.GetAnimatorId();
+
+        GameObject model = Instantiate(Resources.Load<GameObject>(CHARACTER_MODEL_PATH + modeId));
+        model.transform.SetParent(transform);
+        model.transform.position = transform.position;
+        model.transform.localScale = Vector3.one;
+    }
+}
